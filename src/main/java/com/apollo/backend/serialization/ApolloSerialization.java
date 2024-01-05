86a4deb845588fc98f7e1a4ec93b9c4bf6d8cd28
Jacob Franco <jacob@yoapollo.com>
2024-01-05 17:32:05 -0500

    test git

--- src/main/java/com/apollo/backend/serialization/ApolloSerialization.java
+++ src/main/java/com/apollo/backend/serialization/ApolloSerialization.java
@@ -2,7 +2,7 @@ package com.apollo.backend.serialization;
 
 import com.apollo.backend.data.*;
 import java.util.*;
-
+// Test comment
 public class ApolloSerialization extends ThriftSerialization {
 
     @Override
