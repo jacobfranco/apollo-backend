6a451bd9dc28688dc076e3b9de34fecd12438eaf
Jacob Franco <jacobfranco@protonmail.ch>
2024-12-21 19:19:40 -0500

    change frontend url

--- backend/src/main/java/com/apollo/backend/ApolloConfig.java
+++ backend/src/main/java/com/apollo/backend/ApolloConfig.java
@@ -4,5 +4,5 @@ public class ApolloConfig {
     public static String API_URL = System.getProperty("backend.api.url", "http://localhost:8080");
     public static String API_WEB_SOCKET_URL = System.getProperty("backend.api.web.socket.url", "ws://localhost:8080");
     public static String API_DOMAIN = System.getProperty("backend.api.domain", "localhost");
-    public static String FRONTEND_URL = System.getProperty("backend.frontend.url", "http://localhost:4173");
+    public static String FRONTEND_URL = System.getProperty("backend.frontend.url", "http://localhost:3036");
 }
